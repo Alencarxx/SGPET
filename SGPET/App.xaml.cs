27561eab85a2abb3377d2062724324316a9ef112
Alencar Moreira Gonçalves Porto <alencarmoreira@snews.tv>
2017-06-24 19:29:39 -0300

    Implement startup main

--- SGPET/App.xaml.cs
+++ SGPET/App.xaml.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
+using DevExpress.Xpf.Utils.Themes;
+using log4net;
 
 namespace SGPET
 {
@@ -13,5 +18,36 @@ namespace SGPET
     /// </summary>
     public partial class App : Application
     {
+        public static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            try
+            {
+                //StartLog();
+                Log.Info("SGPET starting....");
+                Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
+                //AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+                //GlobalThemeHelper.Instance.ApplicationThemeName = Config.ApplicationThemeName;
+                //SplashScreen.Show();
+                LoadDispatcher(() =>
+                {
+                    MainWindow = new MainWindow();
+                    //MainWindow.Loaded += (sender, args) //=> SplashScreen.Close();
+                    MainWindow.Show();
+                    Log.Info("SGPET started up.");
+                });
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+            }
+        }
+
+        private static void LoadDispatcher(Action action, DispatcherPriority priority = DispatcherPriority.Send)
+        {
+            Current.Dispatcher.BeginInvoke(priority, action);
+        }
+
+
     }
 }
