a5d24f913e6312fb4b84e2d521b7dea89d89d994
Alencar Moreira Gonçalves Porto <alencarmoreira@snews.tv>
2017-06-26 23:02:06 -0300

    Implementação da base de dados, ajustes na homepage para exibir uma lista do dataset e ajustes no agendamento para cadastrar e exibir agendamentos realizados.

--- SGPET/View/AgendamentoWindow.xaml.cs
+++ SGPET/View/AgendamentoWindow.xaml.cs
@@ -23,5 +23,29 @@ namespace SGPET.View
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource agendamentoViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("agendamentoViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // agendamentoViewSource.Source = [generic data source]
+            SGPET.SGPETDADOSDataSet sGPETDADOSDataSet = ((SGPET.SGPETDADOSDataSet)(this.FindResource("sGPETDADOSDataSet")));
+            // Load data into the table Agendamentos. You can modify this code as needed.
+            SGPET.SGPETDADOSDataSetTableAdapters.AgendamentosTableAdapter sGPETDADOSDataSetAgendamentosTableAdapter = new SGPET.SGPETDADOSDataSetTableAdapters.AgendamentosTableAdapter();
+            sGPETDADOSDataSetAgendamentosTableAdapter.Fill(sGPETDADOSDataSet.Agendamentos);
+            System.Windows.Data.CollectionViewSource agendamentosViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("agendamentosViewSource")));
+            agendamentosViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void agendamentosDataGrid_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void agendamentosDataGrid_PreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
