5315506dcbdf3ba48cdeb1405f3fd24a3d3278ac
Alencar Moreira Gonçalves Porto <alencarmoreira@snews.tv>
2017-06-26 21:05:04 -0300

    Implementação window agendamento, vacinas e outros ajustes.

--- SGPET/ViewModel/ToolBarViewModel.cs
+++ SGPET/ViewModel/ToolBarViewModel.cs
@@ -17,29 +17,48 @@ namespace SGPET.ViewModel
             HomeCmd = new RelayCommand<object>(ExecuteHomeCmd, null);
             ClienteCmd = new RelayCommand<object>(ExecuteClienteCmd, null);
             ContratoCmd = new RelayCommand<object>(ExecuteContratoCmd, null);
+            AgendamentoCmd = new RelayCommand<object>(ExecuteAgendamentoCmd, null);
+            VacinaCmd = new RelayCommand<object>(ExecuteVacinaCmd, null);
         }
 
         public ICommand HomeCmd { get; internal set; }
         public ICommand ClienteCmd { get; internal set; }
         public ICommand ContratoCmd { get; internal set; }
+        public ICommand AgendamentoCmd { get; internal set; }
+        public ICommand VacinaCmd { get; internal set; }
+        
 
 
-        public void ExecuteHomeCmd(object parameter)
+        private void ExecuteHomeCmd(object parameter)
         {
 
         }
 
-        public void ExecuteClienteCmd(object parameter)
+        private void ExecuteClienteCmd(object parameter)
         {
             var clientesWidnow = new ClientesWindow();
             clientesWidnow.ShowDialog();
         }
 
-        public void ExecuteContratoCmd(object parameter)
+        private void ExecuteContratoCmd(object parameter)
         {
             var contratosWidnow = new ContratosWindow();
             contratosWidnow.ShowDialog();
         }
+
+        private void ExecuteAgendamentoCmd(object parameter)
+        {
+            var agendamentoWidnow = new AgendamentoWindow();
+            agendamentoWidnow.ShowDialog();
+        }
+
+        private void ExecuteVacinaCmd(object parameter)
+        {
+            var vacinaWidnow = new VacinaWindow();
+            vacinaWidnow.ShowDialog();
+        }
+
+        
     }
 }
 
